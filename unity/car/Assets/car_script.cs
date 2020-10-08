8bf0f0e9f3cf5bdae377436c309c1bd0dd63b43b
Christopher Sahadeo <chris@virtanatech.com>
2020-10-08 18:55:58 -0400

    Added unity.

---
+++ unity/car/Assets/car_script.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_script : MonoBehaviour
+{
+
+    public string name;
+    public bool isActive;
+    public GameObject frontRight;
+    public GameObject frontLeft;
+    public GameObject rearRight;
+    public GameObject rearLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am chris and this is my " + name);
+        // isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.isActive)
+        {
+            return;
+        }
+
+
+        HingeJoint rearLeftHingeJoint = rearLeft.GetComponent<HingeJoint>();
+        HingeJoint rearRightHingeJoint = rearRight.GetComponent<HingeJoint>();
+        HingeJoint frontLeftHingeJoint = frontLeft.GetComponent<HingeJoint>();
+        HingeJoint frontRightHingeJoint = frontRight.GetComponent<HingeJoint>();
+
+        var driveMotor = new JointMotor();
+        var turnMotor = new JointMotor();
+
+        driveMotor.force = 0;
+        driveMotor.targetVelocity = 0;
+        driveMotor.freeSpin = false;
+
+        // set all to zero
+        frontLeftHingeJoint.motor = frontRightHingeJoint.motor = rearLeftHingeJoint.motor = rearRightHingeJoint.motor = driveMotor;
+
+        frontLeftHingeJoint.useMotor = frontRightHingeJoint.useMotor = rearLeftHingeJoint.useMotor = rearRightHingeJoint.useMotor = true;
+
+        turnMotor.freeSpin = true;
+
+        if (Input.GetKey("w"))
+        {
+            driveMotor.force = 1000;
+            driveMotor.targetVelocity = 700;
+
+            rearRightHingeJoint.motor = driveMotor;
+            rearRightHingeJoint.useMotor = true;
+
+            rearLeftHingeJoint.motor = driveMotor;
+            rearLeftHingeJoint.useMotor = true;        
+
+
+        }
+        if (Input.GetKey("s"))
+        {
+            driveMotor.force = 1000;
+            driveMotor.targetVelocity = -700;
+
+            rearRightHingeJoint.motor = driveMotor;
+            rearRightHingeJoint.useMotor = true;
+
+            rearLeftHingeJoint.motor = driveMotor;
+            rearLeftHingeJoint.useMotor = true;
+
+        }
+        if (Input.GetKey("a"))
+        {
+            turnMotor.force = 1000;
+            turnMotor.targetVelocity = 700;
+
+            frontRightHingeJoint.motor = rearRightHingeJoint.motor = turnMotor;
+            frontRightHingeJoint.useMotor = rearRightHingeJoint.useMotor = true;
+        }
+        if (Input.GetKey("d"))
+        {
+            turnMotor.force = 1000;
+            turnMotor.targetVelocity = 700;
+
+            frontLeftHingeJoint.motor = rearLeftHingeJoint.motor = turnMotor;
+            frontLeftHingeJoint.useMotor = rearLeftHingeJoint.useMotor = true;
+        }       
+
+    }
+}
---
+++ unity/car/Assets/setActive.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class setActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    List<GameObject> FindAllPrefabInstances(UnityEngine.Object CarFinal)
+    {
+        List<GameObject> result = new List<GameObject>();
+        GameObject[] allObjects = (GameObject[])FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject GO in allObjects)
+        {
+            if (PrefabUtility.GetPrefabType(GO) == PrefabType.PrefabInstance)
+            {
+                UnityEngine.Object GO_prefab = PrefabUtility.GetPrefabParent(GO);
+                if (CarFinal == GO_prefab)
+                    result.Add(GO);
+            }
+        }
+        return result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (Input.GetKeyDown("" + i.ToString()))
+            {
+                //List<GameObject> cars = FindAllPrefabInstances(CarFinal);
+
+                GameObject[] cars = GameObject.FindGameObjectsWithTag("Player");
+                foreach (GameObject car in cars)
+                {
+                    car_script script = car.GetComponent<car_script>();
+
+                    if (script.name == i.ToString())
+                    {
+                        script.isActive = true;
+                    }
+                    else
+                    {
+                        script.isActive = false;
+                    }
+                }
+            }
+        }
+    }
+}
